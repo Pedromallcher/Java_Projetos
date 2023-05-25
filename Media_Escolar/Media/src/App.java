cb242f46b2c6113535c1f40ca37c60fa07cf050f
Pedromallcher <pedromallcher@gmail.com>
2023-05-25 14:53:55 -0300

    Create App.java

---
+++ Media_Escolar/Media/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner Scanner = new Scanner (System.in);
+
+        System.out.println("Bem-Vindo a Media");
+
+        System.out.println("Digite a nota 1 :");
+        int num1 = Scanner.nextInt();
+        System.out.println("Digite a nota 2 :");
+        int num2 = Scanner.nextInt();
+        System.out.println("Digite a nota 3 :");
+        int num3 = Scanner.nextInt();
+        System.out.println("Digite a nota 4 :");
+        int num4 = Scanner.nextInt();
+
+
+
+
+
+
+    }
+}
