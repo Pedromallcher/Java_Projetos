4dd95fdc9ca34a987912e8b3e82d6bd9a139d7d2
Pedromallcher <pedromallcher@gmail.com>
2023-05-25 14:13:16 -0300

    Update App.java

--- calculadora/src/App.java
+++ calculadora/src/App.java
@@ -1,18 +1,54 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner Scanner = new Scanner(System.in);
 
 
-        String msg = """
-                testeeeeee
+        System.out.print("Seja-Bem vindo ");
 
+        System.out.println("DIgite o valor 1:");;
+        int num1 = Scanner.nextInt();
+        System.out.println("Digite o valor 2: ");
+        int num2 = Scanner.nextInt();
 
-                meu nome e pedro
+        System.out.print("1 - Adicao");
+        System.out.println("2 Subtracao");
+        System.out.println("3 - Multiplicacao");
+        System.out.println("4 - Divisap");
+        int opcao = Scanner.nextInt();
 
-                esse e mais um texte
-                """;
+        int resultado = 0;
 
+        switch (opcao) {
+            case 1:
+                resultado = num1 + num2;
+                System.out.println(resultado);
+                
+             break;
 
+            case 2 :
+                resultado = num1 - num2;
+                System.out.println(resultado);
+            break;
+            case 3: 
+                resultado = num1 * num2;
+                System.out.println(resultado);
+            break;
 
+            case 4:
+            if (num2 !=0){
+                resultado = num1 / num2;
+                System.out.println(resultado);
+            } else {
+                System.out.println("Nao e possivel dividir por 0");
+            }
+            break;
+//
+            default:
+            System.out.println("Nao temos essa opcao em nosso banco de dados");
+                break;
+        }
+        Scanner.close();  
     }
 }
