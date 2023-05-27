3ca2d2e7a0c62ff13bcb097ee03a5f0e7acf9977
Pedromallcher <pedromallcher@gmail.com>
2023-05-27 08:58:31 -0300

    Update App.java

--- calculadora/src/App.java
+++ calculadora/src/App.java
@@ -1,17 +1,3 @@
 import java.util.Scanner;
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-public class App {
-    public static void main (String [] args){
-        Scanner Scanner = new Scanner(System.in);
-
-        System.out.println ("xxxxxxxxxxxxxxxxxxxxxx");
 
 
-        System.out.println ("V1 :");
-        int v1 = Scanner.nextInt();
-
-        System.out.println ("V1 :");
-        int v2 = Scanner.nextInt();
-
-    }
-}
